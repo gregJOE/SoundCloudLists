9a8dea49e99b04324b73850add952c122dcda888
Gregory Job <job.gregory@gmail.com>
2016-11-20 12:02:20 -0500

    init commit

---
+++ SoundCloudLists/SoundCloudLists/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SoundCloudLists.Controllers
+{
+    public class UserController : Controller
+    {
+        public string Detail()
+        {
+            return "detaillll";
+        }
+        public string Delete()
+        {
+
+        }
+    }
+}
